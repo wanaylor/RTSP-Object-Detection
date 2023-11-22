428e385b988eb09a1e462e591c0a78d174609665
Andrew Naylor <wnaylor3@gatech.edu>
2023-11-21 21:55:38 -0500

    finished layer 2 nodes

--- litedetect.py
+++ litedetect.py
@@ -4,24 +4,38 @@ class litedetect(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.a = self.Conv(in_channels=3, out_channels=8, kernel_size=1, stride=2) # down to 320x320
-        self.b = self.Conv(in_channels=8, out_channels=16, kernel_size=1, stride=2) # down to 160x160
-        self.c = self.Conv(in_channels=16, out_channels=8, kernel_size=1, stride=2) # down to 80x80
-        self.d = self.Conv(in_channels=8, out_channels=4, kernel_size=1, stride=2) # down to 40x40
+        # Layer 1
+        self.a = Conv(in_channels=3, out_channels=8, kernel_size=1, stride=2) # down to 320x320x8
+        self.b = Conv(in_channels=8, out_channels=16, kernel_size=1, stride=2) # down to 160x160x16
+        self.c = Conv(in_channels=16, out_channels=32, kernel_size=1, stride=2) # down to 80x80x32
+        self.d = Conv(in_channels=32, out_channels=64, kernel_size=1, stride=2) # down to 40x40x64
+        self.e = Conv(in_channels=64, out_channels=64, kernel_size=1, stride=2) # down to 20x20x64
+        self.f = torch.nn.MaxPool2d(kernel_size=(2,2), stride=(1,1), padding=(1,1)) # still 20x20x64
 
-        self.a_pooled = torch.nn.MaxPool2d(kernel_size=(2,2)) # down to 160x160
-        self.b_pooled = torch.nn.MaxPool2d(kernel_size=(2,2)) # down to 80x80
-        self.c_pooled = torch.nn.MaxPool2d(kernel_size=(2,2)) # down to 40x40
-        self.d_pooled = torch.nn.MaxPool2d(kernel_size=(2,2)) # down to 20x20
+        # Layer 2
+        self.f_up = torch.nn.Upsample(size=(40,40)) # 40x40x64
+        self.d_l2 = Conv(in_channels=128, out_channels=128, kernel_size=1, stride=1)
+        self.d_l2_up = torch.nn.Upsample(size=(80,80)) # 80x80x128
+        self.c_l2 = Conv(in_channels=160, out_channels=160, kernel_size=1, stride=1)
 
 
     def forward(self, x):
+        # Layer 1
         a_out = self.a(x)
         b_out = self.b(a_out)
         c_out = self.c(b_out)
         d_out = self.d(c_out)
+        e_out = self.e(d_out)
+        f_out = self.f(e_out)
 
-        a_pooled = torch.nn.MaxPool2d(kernel_size=(2,2))
+        # Layer 2
+        f_up_out = self.f_up(f_out) # 40x40x64
+        f_d_cat = torch.concat(f_up_out, d_out) # 40x40x128
+        d_l2_out = self.d_l2(f_d_cat)
+        d_l2_up_out = self.d_l2_up(d_l2_out) # 80x80x128
+        d_c_cat = torch.concat(d_l2_up_out, c_out) # 80x80x160
+        c_l2_out = self.c_l2(d_c_cat)
+
 
 class Conv(torch.nn.Module):
     def  __init__(self,in_channels, out_channels, kernel_size, stride, padding):
